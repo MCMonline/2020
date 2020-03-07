a72fe5ac57658d38aa50c5083621e923b27ea03f
Tokamak-307 <52354748+Tokamak-307@users.noreply.github.com>
2020-03-07 16:18:04 +0800

    Fix bug in Fc
    Implemented the new expression of Fc. Check for bugs.

--- sandcastle/Fc.m
+++ sandcastle/Fc.m
@@ -1,5 +1,4 @@
-function outout = Fc ()
+function outout = Fc (theta,theta_d,Gamma)
 r=0.001;%(0.0003~0.0015);
-%outout=1.2*pi*tau*r*0.46*(3*0.64/(4*pi*r^3))^(2/3);
-outout=0.03469/r;
+outout=1.2*pi*Gamma*r*cos(deg2rad(theta_d))*(3*0.64/(4*pi*r^3))^(2/3) / (sqrt(6)*deg2rad(theta-theta_d));
 end
--- sandcastle/main.m
+++ sandcastle/main.m
@@ -4,7 +4,10 @@ clear;
 total = 1000; % 共切分1000个点，每个x向距离为1
 hight = 1; % hight表示层数。暂时只研究一层。
 hh = 500; % 高度
-vv = 0.003;
+vv = 26;
+Gamma = 0.07; % 材料粘度
+theta = 30; % 倾角（度）
+theta_d = 23.4;
 
 global point;
 point = zeros(total,3,hight); % 第2维分别表示x,y,坐标 
@@ -12,14 +15,15 @@ point(1:total,1,1) = 1:total; % 初始化x
 point(1:total,2,1) = gety(total); % 由x计算y，初始设为圆形
 
 %% 迭代
-for tt = 1:100  %迭代100次
+for tt = 1:200  %迭代100次
     %球面插值——用于求切线
     x1 = 0:0.1:total;
     y1 = interp1(point(1:total,1,1),point(1:total,2,1),x1,'spline');
     %优化开始
     for x = 1:total
-        F = geogeo(x,y1(x*10),y1(x*10+1),hh,vv)  %求在切平面上除了Fc外的合力
-        if F > Fc %如果合力超过了Fc的上限
+        F = geogeo(x,y1(x*10),y1(x*10+1),hh,vv,theta,theta_d);  %求在切平面上除了Fc外的合力
+        fc = Fc(theta,theta_d,Gamma);
+        if F > fc %如果合力超过了Fc的上限
             point(x,2,1) = max(point(x,2,1) - 1, 0); % y--
         end
     end 
